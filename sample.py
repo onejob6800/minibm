7ce696897abe5536e85b7ef2de0209965b5e554b
onejob6800 <ultegra_onejob@163.com>
2016-02-26 10:39:09 +0800

    Delete some error output

--- sample.py
+++ sample.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python2
 
 import bitmessage
-import os
 import time
-import sys
 
 bitmessage.start()
 print "Bitmessage started..."
 print "Your address is %s" % bitmessage.getCurrentAddress()
 testaddr = 'BM-2cU1gvauGukBHXPCARmHqfvut2YMFmu8Ce'
 while True:
-    #Construct a message here
     now = time.strftime("%H:%M:%S %Z", time.localtime(time.time()))
+    print now
     message = "Time:" + now + "\n"
     ackdata = bitmessage.sendMessage(testaddr, "Come with some test", message)
-    print "Send system info at %s" % now
     time.sleep(10 * 60)
